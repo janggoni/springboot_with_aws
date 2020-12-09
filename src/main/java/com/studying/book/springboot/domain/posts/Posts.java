2b7bb87a0057783b46fd181e82a5753fdd4e1863
juila00 <57132559+juila00@users.noreply.github.com>
2020-12-09 22:43:11 +0900

    springboot study commit

---
+++ src/main/java/com/studying/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,14 @@
+package com.studying.book.springboot.domain.posts;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+
+}
